050d0bc65c0c6178283598784ed1ca4e42bbbabe
liuyangming <liuyangming@gmail.com>
2017-05-12 16:12:43 +0800

    增加简化配置使用样例

---
+++ springcloud-sample/sample-consumer/src/main/java/com/bytesvc/consumer/controller/SimplifiedController.java
@@ -0,0 +1,68 @@
+package com.bytesvc.consumer.controller;
+
+import org.bytesoft.compensable.Compensable;
+import org.bytesoft.compensable.CompensableCancel;
+import org.bytesoft.compensable.CompensableConfirm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bytesvc.consumer.service.ITransferService;
+import com.bytesvc.feign.service.IAccountService;
+
+@Compensable(interfaceClass = ITransferService.class, simplified = true)
+@RestController
+public class SimplifiedController implements ITransferService {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	private IAccountService acctService;
+
+	@ResponseBody
+	@RequestMapping(value = "/simplified/transfer", method = RequestMethod.POST)
+	@Transactional
+	public void transfer(@RequestParam String sourceAcctId, @RequestParam String targetAcctId, @RequestParam double amount) {
+		this.acctService.decreaseAmount(sourceAcctId, amount);
+		this.increaseAmount(targetAcctId, amount);
+
+		// throw new IllegalStateException("rollback!");
+	}
+
+	private void increaseAmount(String acctId, double amount) {
+		int value = this.jdbcTemplate.update("update tb_account_two set frozen = frozen + ? where acct_id = ?", amount, acctId);
+		if (value != 1) {
+			throw new IllegalStateException("ERROR!");
+		}
+		System.out.printf("exec increase: acct= %s, amount= %7.2f%n", acctId, amount);
+	}
+
+	@CompensableConfirm
+	@Transactional
+	public void confirmTransfer(String sourceAcctId, String targetAcctId, double amount) {
+		int value = this.jdbcTemplate.update(
+				"update tb_account_two set amount = amount + ?, frozen = frozen - ? where acct_id = ?", amount, amount,
+				targetAcctId);
+		if (value != 1) {
+			throw new IllegalStateException("ERROR!");
+		}
+		System.out.printf("done increase: acct= %s, amount= %7.2f%n", targetAcctId, amount);
+	}
+
+	@CompensableCancel
+	@Transactional
+	public void cancelTransfer(String sourceAcctId, String targetAcctId, double amount) {
+		int value = this.jdbcTemplate.update("update tb_account_two set frozen = frozen - ? where acct_id = ?", amount,
+				targetAcctId);
+		if (value != 1) {
+			throw new IllegalStateException("ERROR!");
+		}
+		System.out.printf("exec decrease: acct= %s, amount= %7.2f%n", targetAcctId, amount);
+	}
+
+}
