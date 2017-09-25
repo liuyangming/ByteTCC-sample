e4d3b9018a97733d329ac3ca47bab45e81ff9cfb
liuyangming <liuyangming@gmail.com>
2017-09-25 16:54:29 +0800

    update

--- dubbo-sample/sample-consumer/src/main/resources/ddl.sql
+++ dubbo-sample/sample-consumer/src/main/resources/ddl.sql
@@ -3,6 +3,6 @@ CREATE TABLE tb_account_two (
   amount double(10, 2),
   frozen double(10, 2),
   PRIMARY KEY (acct_id)
-);
+) ENGINE=InnoDB;
 
 insert into tb_account_two (acct_id, amount, frozen) values('2001', 10000.00, 0.00);
