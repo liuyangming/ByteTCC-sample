de9c0cfa2636035303cf02f0290b4deba47c3642
liuyangming <liuyangming@gmail.com>
2017-11-15 17:51:14 +0800

    + 添加hystrix配置

--- springcloud-sample/sample-consumer/src/main/java/com/bytesvc/consumer/main/SampleConsumerMain.java
+++ springcloud-sample/sample-consumer/src/main/java/com/bytesvc/consumer/main/SampleConsumerMain.java
@@ -3,9 +3,12 @@ package com.bytesvc.consumer.main;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ImportResource;
 
 @ImportResource({ "classpath:bytetcc-supports-springcloud.xml", "classpath:spring-mybatis.xml" })
@@ -13,6 +16,9 @@ import org.springframework.context.annotation.ImportResource;
 @EnableEurekaClient
 @EnableFeignClients("com.bytesvc.feign")
 @SpringBootApplication(scanBasePackages = "com.bytesvc.consumer")
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 public class SampleConsumerMain {
 
 	public static void main(String[] args) {
